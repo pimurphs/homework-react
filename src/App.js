f081c612216e3734091bb2c682d326ddee1fbb4b
pimurphs <pi.rodrigues@live.com>
2022-06-14 19:58:05 +0100

    Added link for Github and changed background color

--- src/App.js
+++ src/App.js
@@ -5,8 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>SheCodes Homework</h1>
+        <h1>SheCodes React Week 4 Homework</h1>
+        <h2>Search for a city for the weather! 🌞</h2>
         <Weather />
+        <h3>
+          <a
+            href="https://github.com/pimurphs/homework-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced code
+          </a>{" "}
+          by{" "}
+          <a
+            href="https://www.linkedin.com/in/filipadaguiarrodrigues/"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Filipa Rodrigues
+          </a>
+        </h3>
       </header>
     </div>
   );
